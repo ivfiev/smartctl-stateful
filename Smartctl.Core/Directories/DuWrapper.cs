24942d00a96c15dfb2878574aabfcad5bbd955c5
ivfiev <filjko@ya.ru>
2024-04-21 21:17:34 +0100

    wrap du

---
+++ Smartctl.Core/Directories/DuWrapper.cs
@@ -0,0 +1,45 @@
+using Smartctl.Core.Contracts;
+
+namespace Smartctl.Core.Directories;
+
+public class DuWrapper(ICommandExecutor cmd) : IDirectoryStatsProvider
+{
+    public DirectoryStats[] GetStats(string directory)
+    {
+        directory = directory.TrimEnd('/');
+        var du = GetCommand(directory);
+        var output = cmd.Exec(du);
+        var lines = output.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var stats = lines.Select(line => Parse(directory, line)).ToArray();
+        return stats;
+    }
+
+    private string GetCommand(string dir)
+    {
+        var ls = cmd.Exec($"ls -A {dir}");
+        var directories = ls.Split((char[])null!, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var command = $"du -sh {string.Join(" ", directories.Select(sub => $"{dir}/{sub}"))}";
+        return command;
+    }
+
+    private DirectoryStats Parse(string dir, string line)
+    {
+        var pair = line.Split((char[])null!, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var path = $"{dir}/{pair[1]}";
+
+        var size = pair[0].Last() switch
+        {
+            'K' => ParseNumericPart(pair[0]) / 1000000,
+            'M' => ParseNumericPart(pair[0]) / 1000,
+            'G' => ParseNumericPart(pair[0]),
+            _ => double.Parse(pair[0])
+        };
+
+        return new(path, size);
+
+        double ParseNumericPart(string str)
+        {
+            return double.Parse(str[..^1]);
+        }
+    }
+}
